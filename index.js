7055e494b397eca896f6e96feda053a00052f365
meijiao <meijiao@gmail.com>
2016-12-05 14:45:46 -0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var bodyParser = require ('body-parser');
+var morgan = require ('morgan');
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+//app.use(express.static(path.join(__dirname, "/../../client")));
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
