1a81bd8659970f4e8bf65a1e3eefcd11bdd751a5
meijiao <meijiao@gmail.com>
2016-12-05 15:16:01 -0800

    Add player moves from command line using prompt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require ('body-parser');
 var morgan = require ('morgan');
 var board = require('./board')
+var prompt = require ('prompt');
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
@@ -13,3 +14,31 @@ app.listen(3000, function () {
 });
 
 board.printBoard();
+
+var bool = true;
+
+function promptPlayer () {
+  prompt.start();
+
+  var markers = ['O', 'X'];
+  var marker = '';
+
+  bool ? console.log('Your turn. Where do you want to mark ' + 'O?') :
+  console.log('Your turn. Where do you want to mark ' + 'X?')
+
+  prompt.get(['index'], function (err, result) {
+    // Log the results.
+    bool ? marker = markers[0] : markers [1];
+    console.log('You selected: ' + result.index + 'to mark ' + marker);
+    board.markBoard(result.index, marker)
+    console.log('New Board :')
+    board.printBoard();
+    bool = !bool;
+  });
+
+}
+
+promptPlayer();
+
+
+
